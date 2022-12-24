024774dd9b452a6fbb199ef3e0aaf30deb607203
omerkars78 <81978907+omerkars78@users.noreply.github.com>
2022-12-24 23:13:03 +0300

    ds
    sd

--- eth.py
+++ eth.py
@@ -1,59 +1,19 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
-
-# def eth_2019():
-#     eth_2019_csv = pd.read_table(r"eth-2019.csv",sep=(",")) # data.csv dosyasını oku
-#     eth_2019_list = eth_2019_csv.eth # eth sütununu listeye ata
-#     eth_2019_general_istatistic = eth_2019_csv.describe() # genel istatistikleri al
-#     # Veri çerçevesinden eğitim ve test verilerini ayırın
-#     x = eth_2019_csv[['Months']]  # Tahmin edilen değişkenler
-#     y = eth_2019_csv['eth']  # Tahmin edilen hedef değişken
-#     X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=0)   # 80% eğitim, 20% test
-
-#     # Lineer regresyon modelini oluşturun ve eğitin
-#     model = LinearRegression() # Modeli oluştur
-#     model.fit(X_train, y_train) # Modeli eğit
-
-#     # 2. dereceden polinomal özellikler oluşturun
-#     poly = PolynomialFeatures(degree=2)
-#     X_train_poly = poly.fit_transform(X_train)
-#     X_test_poly = poly.transform(X_test)
-
-#     # Modelin doğruluğunu test verileriyle ölçün
-#     accuracy = model.score(X_test_poly, y_test)
-#     print("Modelin doğruluğu:", accuracy)
-
-#     # Modelin doğruluğunu test verileriyle ölçün
-#     accuracy = model.score(X_test, y_test) # Doğruluğu ölç
-#     print("Modelin doğruluğu:", accuracy) # Doğruluğu yazdır
-
-#     # Lineer Regresyona Göre Önümüzdeki 12 ay için tahminler 
-#     X_future = np.array(range(1, 13)).reshape(-1, 1) # 1-12 arasındaki sayıları diziye ata
-#     predictions = model.predict(X_future)
-#     print("Lineer Regresyona Göre Tahminler:", predictions)
-    
-#     # 2. Dereceden Polinomal Regresyona Göre Önümüzdeki 12 ay için tahminler 
-#     X_future_poly = poly.transform(X_future)
-#     predictions_poly = model.predict(X_future_poly)
-#     print("2. Dereceden Polinomal Regresyona Göre Tahminler:", predictions_poly)
-import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.linear_model import LinearRegression
 
 def eth_2019() -> None:
     # Veri kümenizi pandas veri çerçevesine yükleyin
+    print("*" * 50)
     df = pd.read_csv("eth-2019.csv")
 
     # Veri çerçevesinden eğitim ve test verilerini ayırın
     x = df[['Months']]  # Tahmin edilen değişken
     y = df['eth']  # Tahmin edilen hedef değişken
-    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=0)
 
     # Lineer regresyon modelini oluşturun ve eğitin
     model = LinearRegression()
@@ -61,15 +21,15 @@ def eth_2019() -> None:
 
     # Modelin doğruluğunu test verileriyle ölçün
     accuracy = model.score(X_test, y_test)
-    print(f"Lineer regresyon modelinin doğruluğu: {accuracy:.3f}")
+    # print(f"Lineer regresyon modelinin doğruluğu: {accuracy:.3f}")
 
     # Önümüzdeki 12 ay için tahminler yapın
     X_future = np.array(range(1, 13)).reshape(-1, 1)
-    predictions = model.predict(X_future)
-    print("Lineer regresyona göre tahminler:", predictions)
+    predictions_2019 = model.predict(X_future)
+    print("2019 Yılı Lineer regresyona göre tahminler:", predictions_2019)
 
     # 2. dereceden polinomal özellikler oluşturun
-    poly = PolynomialFeatures(degree=2)
+    poly = PolynomialFeatures(degree=2,include_bias=True)
     X_train_poly = poly.fit_transform(X_train)
     X_test_poly = poly.transform(X_test)
 
@@ -79,34 +39,144 @@ def eth_2019() -> None:
 
     # Modelin doğruluğunu test verileriyle ölçün
     accuracy = model_poly.score(X_test_poly, y_test)
-    print(f"2. dereceden polinomal regresyon modelinin doğruluğu: {accuracy:.3f}")
+    # print(f"2. dereceden polinomal regresyon modelinin doğruluğu: {accuracy:.3f}")
 
     # Önümüzdeki 12 ay için tahminler yapın
     X_future_poly = poly.transform(X_future)
     predictions_poly = model_poly.predict(X_future_poly)
-    print("2. dereceden polinomal regresyona göre tahminler:", predictions_poly)
+    print("2019 Yılı 2. dereceden polinomal regresyona göre tahminler:", predictions_poly)
+    print("*" * 50)
+
+def eth_2020() -> None:
+    print("*" * 50)
+    # Veri kümenizi pandas veri çerçevesine yükleyin
+    df = pd.read_csv("eth-2020.csv")
+
+    # Veri çerçevesinden eğitim ve test verilerini ayırın
+    x = df[['Months']]  # Tahmin edilen değişken
+    y = df['eth']  # Tahmin edilen hedef değişken
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=0)
+
+    # Lineer regresyon modelini oluşturun ve eğitin
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+
+    # Modelin doğruluğunu test verileriyle ölçün
+    accuracy = model.score(X_test, y_test)
+    # print(f"Lineer regresyon modelinin doğruluğu: {accuracy:.3f}")
+
+    # Önümüzdeki 12 ay için tahminler yapın
+    X_future = np.array(range(1, 13)).reshape(-1, 1)
+    predictions_2020 = model.predict(X_future)
+    print("2020 Yılı Lineer regresyona göre tahminler:", predictions_2020)
+
+    # 2. dereceden polinomal özellikler oluşturun
+    poly = PolynomialFeatures(degree=2,include_bias=True)
+    X_train_poly = poly.fit_transform(X_train)
+    X_test_poly = poly.transform(X_test)
+
+    # 2. dereceden polinomal regresyon modelini oluşturun ve eğitin
+    model_poly = LinearRegression()
+    model_poly.fit(X_train_poly, y_train)
+
+    # Modelin doğruluğunu test verileriyle ölçün
+    accuracy = model_poly.score(X_test_poly, y_test)
+    # print(f"2. dereceden polinomal regresyon modelinin doğruluğu: {accuracy:.3f}")
+
+    # Önümüzdeki 12 ay için tahminler yapın
+    X_future_poly = poly.transform(X_future)
+    predictions_poly_2020 = model_poly.predict(X_future_poly)
+    print("2020 Yılı 2. dereceden polinomal regresyona göre tahminler:", predictions_poly_2020)
+    print("*" * 50)
+
+def eth_2021() -> None:
+    print("*" * 50)
+    # Veri kümenizi pandas veri çerçevesine yükleyin
+    df = pd.read_csv("eth-2021.csv")
+
+    # Veri çerçevesinden eğitim ve test verilerini ayırın
+    x = df[['Months']]  # Tahmin edilen değişken
+    y = df['eth']  # Tahmin edilen hedef değişken
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=0)
+
+    # Lineer regresyon modelini oluşturun ve eğitin
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+
+    # Modelin doğruluğunu test verileriyle ölçün
+    accuracy = model.score(X_test, y_test)
+    # print(f"Lineer regresyon modelinin doğruluğu: {accuracy:.3f}")
+
+    # Önümüzdeki 12 ay için tahminler yapın
+    X_future = np.array(range(1, 13)).reshape(-1, 1)
+    predictions_2021 = model.predict(X_future)
+    print("2021 Yılı Lineer regresyona göre tahminler:", predictions_2021)
+
+    # 2. dereceden polinomal özellikler oluşturun
+    poly = PolynomialFeatures(degree=2,include_bias=True)
+    X_train_poly = poly.fit_transform(X_train)
+    X_test_poly = poly.transform(X_test)
+
+    # 2. dereceden polinomal regresyon modelini oluşturun ve eğitin
+    model_poly = LinearRegression()
+    model_poly.fit(X_train_poly, y_train)
+
+    # Modelin doğruluğunu test verileriyle ölçün
+    accuracy = model_poly.score(X_test_poly, y_test)
+    # print(f"2. dereceden polinomal regresyon modelinin doğruluğu: {accuracy:.3f}")
+
+    # Önümüzdeki 12 ay için tahminler yapın
+    X_future_poly = poly.transform(X_future)
+    predictions_poly_2021 = model_poly.predict(X_future_poly)
+    print("2021 Yılı 2. dereceden polinomal regresyona göre tahminler:", predictions_poly_2021)
+    print("*" * 50)
+
+def eth_2022() -> None:
+    print("*" * 50)
+    # Veri kümenizi pandas veri çerçevesine yükleyin
+    df = pd.read_csv("eth-2022.csv")
+
+    # Veri çerçevesinden eğitim ve test verilerini ayırın
+    x = df[['Months']]  # Tahmin edilen değişken
+    y = df['eth']  # Tahmin edilen hedef değişken
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=0)
+
+    # Lineer regresyon modelini oluşturun ve eğitin
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+
+    # Modelin doğruluğunu test verileriyle ölçün
+    accuracy = model.score(X_test, y_test)
+    # print(f"Lineer regresyon modelinin doğruluğu: {accuracy:.3f}")
+
+    # Önümüzdeki 12 ay için tahminler yapın
+    X_future = np.array(range(1, 13)).reshape(-1, 1)
+    predictions_2022 = model.predict(X_future)
+    print("2022 Yılı Lineer regresyona göre tahminler:", predictions_2022)
+
+    # 2. dereceden polinomal özellikler oluşturun
+    poly = PolynomialFeatures(degree=2,include_bias=True)
+    X_train_poly = poly.fit_transform(X_train)
+    X_test_poly = poly.transform(X_test)
+
+    # 2. dereceden polinomal regresyon modelini oluşturun ve eğitin
+    model_poly = LinearRegression()
+    model_poly.fit(X_train_poly, y_train)
+
+    # Modelin doğruluğunu test verileriyle ölçün
+    accuracy = model_poly.score(X_test_poly, y_test)
+    # print(f"2. dereceden polinomal regresyon modelinin doğruluğu: {accuracy:.3f}")
+
+    # Önümüzdeki 12 ay için tahminler yapın
+    X_future_poly = poly.transform(X_future)
+    predictions_poly_2022 = model_poly.predict(X_future_poly)
+    print("2022 Yılı 2. dereceden polinomal regresyona göre tahminler:", predictions_poly_2022)   
+    print("*" * 50)
 
 
-
-def eth_2020():
-    eth_2020_csv = pd.read_table(r"eth-2020.csv",sep=(",")) # data.csv dosyasını oku
-    eth_2020_list = eth_2020_csv.eth # eth sütununu listeye ata
-    eth_2020_general_istatistic = eth_2020_csv.describe() # genel istatistikleri al
-
-    
-
-
-def eth_2021():
-    eth_2021_csv = pd.read_table(r"eth-2021.csv",sep=(",")) # data.csv dosyasını oku
-    eth_2021_list = eth_2021_csv.eth # eth sütununu listeye ata
-    eth_2021_general_istatistic = eth_2021_csv.describe() # genel istatistikleri al
-    return eth_2021_list, eth_2021_general_istatistic
-
-def eth_2022():
-    eth_2022_csv = pd.read_table(r"eth-2022.csv",sep=(",")) # data.csv dosyasını oku
-    eth_2022_list = eth_2022_csv.eth # eth sütununu listeye ata
-    eth_2022_general_istatistic = eth_2022_csv.describe() # genel istatistikleri al
-    return eth_2022_list, eth_2022_general_istatistic
 
 
 eth_2019()
+eth_2020()
+eth_2021()
+eth_2022()
